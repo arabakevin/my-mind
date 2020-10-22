995f3f0c0547ecff8c04d9806cd4338977b64512
arabakevin <34araba@gmail.com>
2020-10-22 19:42:04 +0200

    update position at center of mind map

--- my-mind-node.js
+++ my-mind-node.js
@@ -1066,9 +1066,9 @@ MM.Map.prototype.hide = function() {
 MM.Map.prototype.center = function() {
   var node = this._root.getDOM().node;
   var port = MM.App.portSize;
-  var left = ((port[0] - node.offsetWidth)/2) - 300;
-  var top = (port[1] - node.offsetHeight)/2 - 100;
-  this._moveTo(Math.round(left), Math.round(top));
+  var left = ((port[0] - node.offsetWidth)/2);
+  var top = (port[1] - node.offsetHeight)/2;
+  this._moveTo(Math.round(300), Math.round(150));
 
   return this;
 }
